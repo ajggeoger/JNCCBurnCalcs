b34bd598bbcec27d5bc2d38b8f344834807db537
Al <llansteffan@hotmail.com>
2020-10-19 15:45:48 +0100

    First draft of code that works all the way through

--- testscalecode.py
+++ testscalecode.py
@@ -10,21 +10,25 @@ Contributors:
 # Imports
 #import logging
 import os
+import sys
 import datetime
 import json
 
 import rasterio
 
 def proclist(wd, jsonfile, testimages):
-    #print(wd)
+    '''
+    creates a list of images for testing. 
+    Takes in a working directory path, a json file of all test images,and a list of which images to process from the json.
+    Returns a list of lists stating file names, paths and sizes.
+    '''
+    # TODO: Have this/a function read the files from the data directory in the operational code.... 
     with open(jsonfile) as json_file: 
         data = json.load(json_file)
     data   
 
     outlist = []
     for i in testimages:
-        #print(data[i])
-        #print(data[i]['name'])
         a = data[i]['date']
         year = a[0:4]
         month = a[4:6]
@@ -34,10 +38,8 @@ def proclist(wd, jsonfile, testimages):
         paramlist = (imagename, imagepath, data[i]['granule'], data[i]['datafile'])
         outlist.append(paramlist)
 
-    #print(len(filename))
     #print(filename.count('_'))
     #sensor, date, latlon, granule, orbit, utm, proj, mask, sharp, radiative, srefdem, stdsref = filename.split('_')
-    #print(sensor, granule)
     return outlist
     
 
@@ -113,7 +115,7 @@ def savi(nir, red, L = 0.5):
     return savi
 
 
-def savedata(od, nbr, profile):
+def savedata(od, nbr, profile, name):
     kwds = profile
 
     # Change the format driver for the destination dataset to
@@ -126,8 +128,9 @@ def savedata(od, nbr, profile):
     #kwds['blockxsize'] = 256
     #kwds['blockysize'] = 256
     #kwds['compress'] = 'JPEG'
+    outname = name + '.tif'
 
-    with rasterio.open((os.path.join(od, 'second.tif')), 'w', **kwds) as dst_dataset:
+    with rasterio.open((os.path.join(od, outname)), 'w', **kwds) as dst_dataset:
         # Write data to the destination dataset.
         dst_dataset.write(nbr, 1)
 
@@ -148,8 +151,8 @@ if __name__ == "__main__":
     # Set path to jsonfile of images
     jsonfile = '/home/al/sdaDocuments/Code/geoger/github/JNCCBurnCalcs/testimages_T30VVJ.json'
     # Set testimages 
-    testimages = ['TEST_21']#,'TEST_22']
-    #image = 'S2A_20190627_lat57lon375_T30VVJ_ORB080_utm30n_osgb_vmsk_sharp_rad_srefdem_stdsref.tif'
+    testimages = ['TEST_21','TEST_22']
+    
 
     # Get data
     toprocess = proclist(wd, jsonfile, testimages)
@@ -163,40 +166,93 @@ if __name__ == "__main__":
     # Start timer
     starttime1 = datetime.datetime.now()
     print('--STARTING PROCESSING--')
-    for k in toprocess:
-        image_data = os.path.join(wd, k[1], k[0])
+
+
+    cleanlist = []
+    for j in toprocess:
+        if 'GB' in j[3]:
+            cleanlist.append(j)
+
+    # If too few images for comparison, exit the program
+    if len(cleanlist) < 2:
+            print('--EXITING--')
+            print('Too few images to process in test')
+            sys.exit()
+
+    #for k in reversed(cleanlist):
+    
+        #image_data = os.path.join(wd, k[1], k[0])
         #print(image_data)
-    
-        print('--GETTING DATA--')
-        # pre-fire image
-        prered, prenir, preswir1, preswir2, preprofile = pre(image_data)
-        # print(preprofile)
+
+    print('--GETTING DATA--')
+    count = 1
+#j = ['a','b','c','d','e']
+    while len(cleanlist) > 0:
+        if count == 1:
+            postlist = cleanlist.pop()
+            #print('post', post)
+        count = 2
+        prelist = cleanlist.pop()
+        #print('pre', pre)
+
+        #PROCESSING
         # post-fire image
-        #postred, postnir, postswir1, postswir2, postprofile = post(image_data)
-    
-        # print('--CALCULATING NBR--')
-        # prenbr = nbr(preswir1, prenir)
-        # print("Pre-NBR Shape: ", prenbr.shape)
-        # #postnbr = nbr(postswir1, postnir)
-        # #print("Post-NBR Shape: ", postnbr.shape)
+        postred, postnir, postswir1, postswir2, postprofile = post(os.path.join(wd, postlist[1], postlist[0]))
+
+        # pre-fire image
+        prered, prenir, preswir1, preswir2, preprofile = pre(os.path.join(wd, prelist[1], prelist[0]))
+
+        print('--CALCULATING NBR--')
+        prenbr = nbr(preswir1, prenir)
+        #print("Pre-NBR Shape: ", prenbr.shape)
+        postnbr = nbr(postswir1, postnir)
+        #print("Post-NBR Shape: ", postnbr.shape)
+        dnbr = postnbr - prenbr
+
 
         print('--CALCULATING NBR2--')
         prenbr2 = savi(preswir2, preswir1)
-        print("Pre-NBR2 Shape: ", prenbr2.shape)
-        # #postnbr2 = nbr(postswir2, postswir1)
-        # #print("Post-NBR2 Shape: ", postnbr.shape)
+        #print("Pre-NBR2 Shape: ", prenbr2.shape)
+        postnbr2 = nbr(postswir2, postswir1)
+        #print("Post-NBR2 Shape: ", postnbr.shape)
+        dnbr2 = postnbr2 - prenbr2
+
 
         print('--CALCULATING SAVI--')
-        presavi = savi(prenir, prered)
-        # print("Pre-SAVI Shape: ", presavi.shape)
-        # #postsavi = savi(postnir, postred)
-        # #print("Pre-SAVI Shape: ", postsavi.shape)
+        #presavi = savi(prenir, prered)
+        #print("Pre-SAVI Shape: ", presavi.shape)
+        #postsavi = savi(postnir, postred)
+        #print("Pre-SAVI Shape: ", postsavi.shape)
+        dsavi = savi(postnir, postred) - savi(prenir, prered)
 
-    # #TODO: difference images
+
     # #TODO: Thresholding
 
-        print('--WRITING OUTPUT--')
-        savedata(od, prenbr2, preprofile)
+
+
+        if len(cleanlist) >= 1:
+            postlist = prelist
+            #print('post', post)
+    
+
+    
+    #while len(cleanlist) > 1:
+        
+        #postlist = cleanlist.pop()
+
+
+    
+
+        # print(preprofile)
+
+
+
+
+    print('--WRITING OUTPUT--')
+    #savedata(od, prenbr2, preprofile)
+    savedata(od, dnbr2, preprofile, 'dnbr2')
+    savedata(od, dsavi, preprofile, 'dsavi')
+
 
     # Stop timer
     endtime1=datetime.datetime.now()
